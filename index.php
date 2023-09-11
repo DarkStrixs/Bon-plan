05dd48e99e18cfad7e772eba3773373a4fb844e0
DarkStrixs <98765897+DarkStrixs@users.noreply.github.com>
2023-09-11 09:03:32 +0000

    V0.1

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les bons plans inconnu d'Amazon pas DarkStrixs</title>
+    <link rel="stylesheet" href="./styles/stylesindex.css">
+</head>
+<body>
+    
+</body>
+<footer>
+    <p>DarkStrixs 2023</p>
+</footer>
+</html>
